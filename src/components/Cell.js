19578e7b10eb9f4d4448823390c37e774f133dc1
adamak93 <mail@adamkhafagy.com>
2019-09-13 01:48:33 -0400

    Fixed scoring issue yay

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from '../tetrominos';
 
+
 const Cell = ({ type }) => (
-    <StyledCell type={type} color={TETROMINOS[type].color}></StyledCell>
-)
+  <StyledCell type={type} color={TETROMINOS[type].color}>
+  </StyledCell>
+);
 
 export default React.memo(Cell);
